b8f2a42313272ded4aee92cf2a2bfe4daa200159
Jorja Fleming <aset284@gmail.com>
2023-05-18 10:20:15 -0600

    Completed assignment

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -61,4 +61,43 @@ RSpec.describe House do
     house = House.new("$400000", "123 sugar lane")
     expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
   end
+
+  it "can get price per square foot" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    expect(house.price_per_square_foot).to eq(210.53)
+  end
+
+  it "can get rooms sorted by area" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
+  end
+
+  it "can get rooms by category" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    expect(house.rooms_by_category).to eq({:bedroom=>[room_1, room_2], :living_room=> [room_3], :basement=> [room_4]})
+  end
 end
