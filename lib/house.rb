70010b30703a9b3b88297273f6bade7629ce63c3
Jorja Fleming <aset284@gmail.com>
2023-05-18 10:00:10 -0600

    Completed Iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -16,4 +16,24 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    price > 500000 ? true : false
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    @rooms.sum do |room|
+      room.area
+    end
+  end
+
+  def details
+    {"price" => price, "address" => address}
+  end
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -24,4 +24,41 @@ RSpec.describe House do
     expect(house.rooms).to eq([room_1, room_2])
   end
   
+  it "can check if above market average" do
+    house = House.new("$400000", "123 sugar lane")
+    expect(house.above_market_average?).to eq(false)  
+  end
+
+  it "can get rooms from category" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')    
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)    
+    house.add_room(room_3)
+    house.add_room(room_4)
+    house.rooms_from_category(:bedroom)
+    expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+    expect(house.rooms_from_category(:basement)).to eq([room_4])
+  end
+
+  it "can get area of all the rooms" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')    
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)    
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expect(house.area).to eq(1900)
+  end
+
+  it "can get details" do
+    house = House.new("$400000", "123 sugar lane")
+    expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+  end
 end
